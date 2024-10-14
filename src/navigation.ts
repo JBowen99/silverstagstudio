96709c5e1cd91804985cbed67ba464f079e47bed
JBowen99 <josephwbowen99@gmail.com>
2024-10-14 17:44:51 -0600

    Removed social links, updated contact email

--- src/navigation.ts
+++ src/navigation.ts
@@ -175,6 +175,8 @@ export const footerData = {
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Email', icon: 'tabler:mail', href: 'mailto:hello@silverstagstudios.com'}
+    { ariaLabel: 'Phone', icon: 'tabler:phone', href: 'tel:7206639079'}
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
